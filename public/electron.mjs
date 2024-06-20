fa7be64c8f0f75bcfbdc132f6ae1b2823b3b229f
anshjain16 <anshjain0216@gmail.com>
2024-06-20 12:06:19 +0530

    init

---
+++ public/electron.mjs
@@ -0,0 +1,35 @@
+import { app, BrowserWindow } from "electron";
+import { initialize } from "@electron/remote/main/index.js";
+
+initialize();
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+    },
+  });
+
+  mainWindow.loadURL("http://localhost:3000");
+
+  mainWindow.on("closed", () => (mainWindow = null));
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
